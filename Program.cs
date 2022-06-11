548e4e148b4440cde151d3803ea40666d2bf61a6
Yaşar Küskü <yasarkusku@hotmail.com>
2022-06-11 14:28:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelefonRehberi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
